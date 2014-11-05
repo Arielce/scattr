9d694d72ae375e446c72d791c2391b041a5ab017
Julien Fouilhé <julien.fouilhe@gmail.com>
2014-11-05 14:03:14 +1100

    Use topic exchange (former was fanout)

--- src/AMQPHandler.cpp
+++ src/AMQPHandler.cpp
@@ -55,7 +55,7 @@ AMQPHandler::onConnected(AMQP::Connection *connection)
   std::cout << "Connected to AMQP (valid credentials)" << std::endl;
   channel_ = std::unique_ptr<AMQP::Channel>(new AMQP::Channel(connection));
   channel_->setQos(QUALITY_OF_SERVICE);
-  channel_->declareExchange(EXCHANGE_NAME).onSuccess([&]() {
+  channel_->declareExchange(EXCHANGE_NAME, AMQP::ExchangeType::topic).onSuccess([&]() {
     std::cout << "Exchange " << EXCHANGE_NAME << " declared successfully" << std::endl;
     channel_->declareQueue(QUEUE_NAME, AMQP::durable).onSuccess([&]() {
       std::cout << "Queue " << QUEUE_NAME << " declared successfully" << std::endl;
