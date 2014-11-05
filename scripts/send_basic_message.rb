4d56d5e0d21c60e3b1f046486efad30463818693
Julien Fouilhé <julien.fouilhe@gmail.com>
2014-11-05 14:26:47 +1100

    use topic exchange in script too

--- scripts/send_basic_message.rb
+++ scripts/send_basic_message.rb
@@ -6,7 +6,7 @@ conn = Bunny.new
 conn.start
 
 ch   = conn.create_channel
-x    = ch.fanout("nb_notifier_exch")
+x    = ch.topic("nb_notifier_exch")
 q    = ch.queue("nb_notifier_queue", { durable: true })
 x.publish("Hello World!", :routing_key => 'Android')
 puts " [x] Sent 'Hello World!'"
