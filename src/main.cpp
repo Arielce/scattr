df44249315c9e083ee65141913bf285b92f219dc
Julien Fouilhé <julien.fouilhe@gmail.com>
2014-11-06 23:14:26 +1100

    use init functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,28 @@
 #include "AdaptersFactory.hh"
 #include "AMQPHandler.hh"
 
+# define TIME_REFRESH_IN_MS 1000
+
 int main(int ac, char** av, char** env)
 {
   try
   {
     Configuration conf(ac, av, env);
+
     if (conf.count("help"))
     {
       std::cout << conf << std::endl;
       return EXIT_SUCCESS;
     }
+    AdaptersFactory::getInstance()->initAdapters(conf);
+
     AMQPHandler handler(conf);
     while (42)
     {
-      boost::this_thread::sleep(boost::posix_time::milliseconds(500));
+      boost::this_thread::sleep(boost::posix_time::milliseconds(TIME_REFRESH_IN_MS));
+      // refresh adapters
       for (auto & adapter : *AdaptersFactory::getInstance())
-      {
         adapter.second->refresh();
-      }
     }
   }
   catch (Configuration::Error & e)
