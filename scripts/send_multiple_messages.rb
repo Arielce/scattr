fd3f22d380b9553d7e97c6de395cdc2ebcdd43c2
Julien Fouilhé <julien.fouilhe@gmail.com>
2014-11-06 23:32:40 +1100

    Send multiple messages script

---
+++ scripts/send_multiple_messages.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "bunny"
+conn = Bunny.new
+conn.start
+
+keys = ['android', 'ios']
+
+ch   = conn.create_channel
+x    = ch.topic("nb_notifier_exch")
+
+[*0..20].each do |i|
+  key = keys.sample
+  x.publish("Hello World!", :routing_key => key, persistent: true)
+  puts " [x] Sent 'Hello World on #{key} queue!'"
+end
+
+conn.close
