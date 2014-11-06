c863a90c00ceb09b9121cb28e54f7dac798c88bb
Julien Fouilhé <julien.fouilhe@gmail.com>
2014-11-06 23:32:55 +1100

    Don't need queue

--- scripts/send_basic_message.rb
+++ scripts/send_basic_message.rb
@@ -7,8 +7,8 @@ conn.start
 
 ch   = conn.create_channel
 x    = ch.topic("nb_notifier_exch")
-q    = ch.queue("nb_notifier_queue", { durable: true })
-x.publish("Hello World!", :routing_key => 'Android', persistent: true)
+
+x.publish("Hello World!", :routing_key => 'android', persistent: true)
 puts " [x] Sent 'Hello World on Android queue!'"
 
 conn.close
